9f40ebbd0087de17cd3833721ceacfbff1e7bd62
ghostSI <ghostsi@reaperforge.com>
2022-02-25 02:21:33 +0100

    working on decryption.

--- src/psarc.cpp
+++ src/psarc.cpp
@@ -8,64 +8,57 @@ static constexpr const char* magicText = "PSAR";
 
 static u32 u32BigEndian(const u8* bytes)
 {
-    return bytes[0] << 24 | bytes[1] << 16 | bytes [2] << 8 | bytes[3];
+  return bytes[0] << 24 | bytes[1] << 16 | bytes[2] << 8 | bytes[3];
 }
 
 std::vector<u8> Psarc::readPsarcData(const char* filepath)
 {
-    const size_t filepathLen = strlen(filepath);
+  const size_t filepathLen = strlen(filepath);
 
-    ASSERT(filepathLen > sizeof(psarcExtension) && "Psarc filename invalid");
-    ASSERT(strncmp(filepath, psarcExtension, filepathLen - sizeof(psarcExtension)));
+  ASSERT(filepathLen > sizeof(psarcExtension) && "Psarc filename invalid");
+  ASSERT(strncmp(filepath, psarcExtension, filepathLen - sizeof(psarcExtension)));
 
-    std::vector<u8> psarcData = File::load(filepath, "r");
+  std::vector<u8> psarcData = File::load(filepath, "r");
 
-    ASSERT(strncmp(reinterpret_cast<char*>(psarcData.data()), magicText, sizeof(magicText)) == 0 && "Invalid Psarc content");
+  ASSERT(strncmp(reinterpret_cast<char*>(psarcData.data()), magicText, sizeof(magicText)) == 0 && "Invalid Psarc content");
 
-    return psarcData;
+  return psarcData;
 }
 
 static const u8 psarcKey[32] =
-        {
-                0xC5, 0x3D, 0xB2, 0x38, 0x70, 0xA1, 0xA2, 0xF7,
-                0x1C, 0xAE, 0x64, 0x06, 0x1F, 0xDD, 0x0E, 0x11,
-                0x57, 0x30, 0x9D, 0xC8, 0x52, 0x04, 0xD4, 0xC5,
-                0xBF, 0xDF, 0x25, 0x09, 0x0D, 0xF2, 0x57, 0x2C
-        };
+{
+  0xC5, 0x3D, 0xB2, 0x38, 0x70, 0xA1, 0xA2, 0xF7,
+  0x1C, 0xAE, 0x64, 0x06, 0x1F, 0xDD, 0x0E, 0x11,
+  0x57, 0x30, 0x9D, 0xC8, 0x52, 0x04, 0xD4, 0xC5,
+  0xBF, 0xDF, 0x25, 0x09, 0x0D, 0xF2, 0x57, 0x2C
+};
 
 static std::vector<u8> decryptPsarc(const u8* bytes, const u32 len)
 {
+  std::vector<u8> plainText(len);
 
-    RIJNDAEL_context ctx{};
-    ctx.mode = MODE_CFB;
+  Rijndael rijndael;
+  rijndael.MakeKey(psarcKey);
+  rijndael.Decrypt(bytes, plainText.data(), 1024);
 
-    rijndael_setup(&ctx, 128, psarcKey);
-
-    std::vector<u8> plainText(len);
-
-    for (size_t i = 0; i < len; i+= 16)
-    {
-        rijndael_decrypt(&ctx, &bytes[i], &plainText[i]);
-    }
-
-    return plainText;
+  return plainText;
 }
 
 Psarc::PsarcInfo Psarc::read(const std::vector<u8>& psarcData)
 {
-    PsarcInfo psarcInfo;
+  PsarcInfo psarcInfo;
 
-    psarcInfo.header.magicNumber = u32BigEndian(&psarcData[0]);
-    ASSERT(psarcInfo.header.magicNumber == 1347633490_u32 && "Invalid Psarc content");
-    psarcInfo.header.version = u32BigEndian(&psarcData[4]);
-    psarcInfo.header.compressMethod = u32BigEndian(&psarcData[8]);
-    psarcInfo.header.totalTocSize = u32BigEndian(&psarcData[12]);
-    psarcInfo.header.TOCEntrySize = u32BigEndian(&psarcData[16]);
-    psarcInfo.header.numFiles = u32BigEndian(&psarcData[20]);
-    psarcInfo.header.BlockSizeAlloc = u32BigEndian(&psarcData[24]);
-    psarcInfo.header.archiveFlags = u32BigEndian(&psarcData[28]);
+  psarcInfo.header.magicNumber = u32BigEndian(&psarcData[0]);
+  ASSERT(psarcInfo.header.magicNumber == 1347633490_u32 && "Invalid Psarc content");
+  psarcInfo.header.version = u32BigEndian(&psarcData[4]);
+  psarcInfo.header.compressMethod = u32BigEndian(&psarcData[8]);
+  psarcInfo.header.totalTocSize = u32BigEndian(&psarcData[12]);
+  psarcInfo.header.TOCEntrySize = u32BigEndian(&psarcData[16]);
+  psarcInfo.header.numFiles = u32BigEndian(&psarcData[20]);
+  psarcInfo.header.BlockSizeAlloc = u32BigEndian(&psarcData[24]);
+  psarcInfo.header.archiveFlags = u32BigEndian(&psarcData[28]);
 
-    decryptPsarc(&psarcData[32], psarcInfo.header.totalTocSize);
+  decryptPsarc(&psarcData[32], psarcInfo.header.totalTocSize);
 
-    return  psarcInfo;
+  return  psarcInfo;
 }
--- src/rijndael.h
+++ src/rijndael.h
@@ -1,116 +1,34 @@
-/* rijndael - An implementation of the Rijndael cipher.
- * Copyright (C) 2000 Rafael R. Sevilla <sevillar@team.ph.inter.net>
- *
- * Currently maintained by brian d foy, <bdfoy@cpan.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the Free
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-/*
- * Rijndael is a 128/192/256-bit block cipher that accepts key sizes of
- * 128, 192, or 256 bits, designed by Joan Daemen and Vincent Rijmen.  See
- * http://www.esat.kuleuven.ac.be/~rijmen/rijndael/ for details.
- */
-
-#if !defined(RIJNDAEL_H)
+#ifndef RIJNDAEL_H
 #define RIJNDAEL_H
 
-#include <stdint.h>
-#include <stdlib.h>
-#include <sys/types.h>
+#include "typedefs.h"
 
-/* Other block sizes and key lengths are possible, but in the context of
- * the ssh protocols, 256 bits is the default. 
- */
-#define RIJNDAEL_BLOCKSIZE 16
-#define RIJNDAEL_KEYSIZE   32
+class Rijndael
+{
+public:
+    void MakeKey(u8 const* key);
 
-#define     MODE_ECB        1    /*  Are we ciphering in ECB mode?   */
-#define     MODE_CBC        2    /*  Are we ciphering in CBC mode?   */
-#define     MODE_CFB        3    /*  Are we ciphering in 128-bit CFB mode? */
-#define     MODE_PCBC       4    /*  Are we ciphering in PCBC mode? */
-#define     MODE_OFB        5    /*  Are we ciphering in 128-bit OFB mode? */
-#define     MODE_CTR        6    /*  Are we ciphering in counter mode? */
+public:
+    void EncryptBlock(u8 const* in, u8* result);
 
-/* Allow keys of size 128 <= bits <= 256 */
+    void Decrypt(u8 const* in, u8* result, size_t n);
 
-#define RIJNDAEL_MIN_KEYSIZE 16
-#define RIJNDAEL_MAX_KEYSIZE 32
+private:
+    //Encryption (m_Ke) round key
+    int m_Ke[15][8];
+    //Decryption (m_Kd) round key
+    int m_Kd[15][8];
+    //Key Length
+    int m_keylength;
+    //Block Size
+    //Number of Rounds
+    //Chain Block
+    u8 m_chain0[32];
+    u8 m_chain[32];
+    //Auxiliary private use buffers
+    int tk[8];
+    int a[8];
+    int t[8];
+};
 
-typedef struct {
-    uint32_t keys[60];		/* maximum size of key schedule */
-    uint32_t ikeys[60];		/* inverse key schedule */
-    int nrounds;			/* number of rounds to use for our key size */
-    int mode;			/* encryption mode */
-} RIJNDAEL_context;
-
-/* This basically performs Rijndael's key scheduling algorithm, as it's the
- * only initialization required anyhow.   The key size is specified in bytes,
- * but the only valid values are 16 (128 bits), 24 (192 bits), and 32 (256
- * bits).  If a value other than these three is specified, the key will be
- * truncated to the closest value less than the key size specified, e.g.
- * specifying 7 will use only the first 6 bytes of the key given.  DO NOT
- * PASS A VALUE LESS THAN 16 TO KEYSIZE! 
- */
-void
-rijndael_setup(RIJNDAEL_context *ctx, size_t keysize, const uint8_t *key);
-
-/*
- * rijndael_encrypt()
- *
- * Encrypt 16 bytes of data with the Rijndael algorithm.  Before this
- * function can be used, rijndael_setup must be used in order to initialize
- * Rijndael's key schedule.
- *
- * This function always encrypts 16 bytes of plaintext to 16 bytes of
- * ciphertext.  The memory areas of the plaintext and the ciphertext can
- * overlap.
- */
-
-void
-rijndael_encrypt(RIJNDAEL_context *context,
-                 const uint8_t *plaintext,
-                 uint8_t *ciphertext);
-
-/*
- * rijndael_decrypt()
- *
- * Decrypt 16 bytes of data with the Rijndael algorithm.
- *
- * Before this function can be used, rijndael_setup() must be used in order
- * to set up the key schedule required for the decryption algorithm.
- * 
- * This function always decrypts 16 bytes of ciphertext to 16 bytes of
- * plaintext.  The memory areas of the plaintext and the ciphertext can
- * overlap.
- */
-
-void
-rijndael_decrypt(RIJNDAEL_context *context,
-                 const uint8_t *ciphertext,
-                 uint8_t *plaintext);
-
-/* Encrypt a block of plaintext in a mode specified in the context */
-void
-block_encrypt(RIJNDAEL_context *ctx, uint8_t *input, int inputlen,
-              uint8_t *output, uint8_t *iv);
-
-/* Decrypt a block of plaintext in a mode specified in the context */
-void
-block_decrypt(RIJNDAEL_context *ctx, uint8_t *input, int inputlen,
-              uint8_t *output, uint8_t *iv);
-
-
-#endif /* RIJNDAEL_H */
+#endif // RIJNDAEL_H

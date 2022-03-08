6b26bdb7347abc6f2cf7decb37b55144ed9c0d58
ghostSI <ghostsi@reaperforge.com>
2022-03-08 23:21:49 +0100

    added webassembly build

---
+++ website/build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cd ../3rdparty/emsdk/
+source ./emsdk_env.sh
+cd ../../website
+emcc -std=c++20 -O3 -flto -funsafe-math-optimizations -fno-exceptions -fno-rtti ../src/*.cpp -s USE_SDL=2 -o index.js \
+--embed-file ../res/icon.ico
--- website/downloadEmscripten.sh
+++ website/downloadEmscripten.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-cd 3rdparty
+cd ../3rdparty
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 git pull
 ./emsdk install latest
 ./emsdk activate latest
-source ./emsdk_env.sh
+./source ./emsdk_env.sh

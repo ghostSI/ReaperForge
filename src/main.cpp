005a2608efcadc4d81c77d33f59e1e9f63bc6bd8
ghostSI <ghostsi@reaperforge.com>
2022-05-07 10:32:26 +0200

    working on webassembly build

--- src/main.cpp
+++ src/main.cpp
@@ -35,13 +35,13 @@ static void mainloop() {
 
   {
     Input::prePollEvent();
-    Ui::handleInputBegin();
+    //Ui::handleInputBegin();
     SDL_Event event;
     while (SDL_PollEvent(&event) != 0) {
       Input::pollEvent(event);
-      Ui::handleInput(event);
+      //Ui::handleInput(event);
     }
-    Ui::handleInputEnd();
+    //Ui::handleInputEnd();
     Input::postPollEvent();
 
     Input::proccessInputEvents();
@@ -51,7 +51,7 @@ static void mainloop() {
   { // render frame
     Sound::tick();
     Camera::tick();
-    Ui::tick();
+    //Ui::tick();
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -62,16 +62,96 @@ static void mainloop() {
       glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 #endif // __EMSCRIPTEN__
 
-    Debug::render();
+    //Debug::render();
     //Highway::render();
-    Camera::render();
-    Ui::render();
+    //Camera::render();
+    //Ui::render();
 
     SDL_GL_SwapWindow(Global::window);
 
     last_frame = current_frame;
   }
 }
+
+//int main(int argc, char* argv[]) {
+//  Installer::init();
+//  if (!Settings::init(argc, argv))
+//    return -1;
+
+//  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER)) {
+//    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+//    SDL_Quit();
+//  }
+
+//  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+//  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+//  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+//  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+//  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+//  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+//  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+//  Global::window = SDL_CreateWindow("ReaperForge", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+//    Global::settings.graphicsResolutionWidth, Global::settings.graphicsResolutionHeight,
+//    SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+
+//  if (Global::window == nullptr) {
+//    SDL_Log("Unable to create Window: %s", SDL_GetError());
+//    SDL_Quit();
+//  }
+
+//  SDL_GLContext con = SDL_GL_CreateContext(Global::window);
+//  SDL_GL_SetSwapInterval(0); // disable vsync
+
+//  //glEnable(GL_CULL_FACE);
+//  //glCullFace(GL_FRONT);
+//  //glFrontFace(GL_CCW);
+//  glEnable(GL_DEPTH_TEST);
+//  glDepthFunc(GL_LESS);
+//  glEnable(GL_BLEND);
+//  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+//  glEnable(GL_SCISSOR_TEST);
+
+//  OpenGl::init();
+//  glGenVertexArrays(1, &Global::vao);
+//  glGenBuffers(1, &Global::vbo);
+//  glGenBuffers(1, &Global::ebo);
+//  glBindVertexArray(Global::vao);
+//  glBindBuffer(GL_ARRAY_BUFFER, Global::vbo);
+//  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Global::ebo);
+//  glGenFramebuffers(1, &Global::fboRtt);
+//  Global::texture = OpenGl::loadDDSTexture(Data::Texture::texture, sizeof(Data::Texture::texture));
+
+//  if (Global::gameController == nullptr && SDL_NumJoysticks() >= 1 && SDL_IsGameController(0))
+//    Global::gameController = SDL_GameControllerOpen(0);
+
+//  glClearColor(0.0f, 0.0031372f, 0.0721568f, 1.0f);
+
+//  Shader::init();
+//  Sound::init();
+//  Camera::init();
+//  //Highway::init();
+//  Font::init();
+//  Ui::init();
+
+//#ifdef __EMSCRIPTEN__
+//  emscripten_set_main_loop(mainloop, -1, 1);
+//#else
+//  while (!Global::appQuit)
+//    mainloop();
+//#endif // __EMSCRIPTEN__
+
+//  Settings::fini();
+
+//  SDL_Quit();
+//  return 0;
+//}
+
+
+
+
+
+
 
 int main(int argc, char* argv[]) {
   Installer::init();
@@ -85,14 +165,18 @@ int main(int argc, char* argv[]) {
 
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
   SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+//#ifdef __EMSCRIPTEN__
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+//#else
+//  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+//  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+//  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+//#endif // __EMSCRIPTEN__
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
   Global::window = SDL_CreateWindow("ReaperForge", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-    Global::settings.graphicsResolutionWidth, Global::settings.graphicsResolutionHeight,
+    800, 600,
     SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
 
   if (Global::window == nullptr) {
@@ -101,7 +185,9 @@ int main(int argc, char* argv[]) {
   }
 
   SDL_GLContext con = SDL_GL_CreateContext(Global::window);
+#ifndef __EMSCRIPTEN__
   SDL_GL_SetSwapInterval(0); // disable vsync
+#endif // __EMSCRIPTEN__
 
   //glEnable(GL_CULL_FACE);
   //glCullFace(GL_FRONT);
@@ -125,9 +211,9 @@ int main(int argc, char* argv[]) {
   if (Global::gameController == nullptr && SDL_NumJoysticks() >= 1 && SDL_IsGameController(0))
     Global::gameController = SDL_GameControllerOpen(0);
 
-  glClearColor(0.0f, 0.0031372f, 0.0721568f, 1.0f);
+  glClearColor(0.0f, 1.0031372f, 0.0721568f, 1.0f);
 
-  Shader::init();
+  //Shader::init();
   Sound::init();
   Camera::init();
   //Highway::init();

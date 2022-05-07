7026b8cc719ad0c811331dc502b05002c0b3797d
ghostSI <ghostsi@reaperforge.com>
2022-05-07 14:45:31 +0200

    emscripten increase max memory

--- website/build.sh
+++ website/build.sh
@@ -5,6 +5,6 @@ cp -r ../songs .
 cd ../3rdparty/emsdk/
 source ./emsdk_env.sh
 cd ../../website
-emcc -std=c++20 -O3 -flto -funsafe-math-optimizations -fno-exceptions -fno-rtti ../src/*.cpp -s USE_SDL=2 -s USE_WEBGL2=1 -s ASSERTIONS=1 -s ALLOW_MEMORY_GROWTH=1 -o index.js \
+emcc -std=c++20 -O3 -flto -funsafe-math-optimizations -fno-exceptions -fno-rtti ../src/*.cpp -s USE_SDL=2 -s USE_WEBGL2=1 -s ASSERTIONS=1 -s MAXIMUM_MEMORY=4GB -s ALLOW_MEMORY_GROWTH=1 -o index.js \
 --embed-file res/icon.ico \
 --embed-file songs/test.psarc

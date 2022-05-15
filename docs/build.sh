7ffc8c58bd64176b210acb0be4aed6b326672c47
GhostSI <ghostsi@reaperforge.com>
2022-05-15 23:38:36 +0200

    fixed path in build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -4,7 +4,7 @@ cp -r ../res .
 cp -r ../songs .
 cd ../3rdparty/emsdk/
 source ./emsdk_env.sh
-cd ../../website
+cd ../../docs
 emcc -std=c++20 -O3 -flto -funsafe-math-optimizations -fno-exceptions -fno-rtti ../src/*.cpp -s USE_SDL=2 -s USE_WEBGL2=1 -s ASSERTIONS=1 -s MAXIMUM_MEMORY=4GB -s ALLOW_MEMORY_GROWTH=1 -o index.js \
 --embed-file res/icon.ico \
 --embed-file songs/test.psarc

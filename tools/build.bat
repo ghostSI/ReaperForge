7e0063f40b4848e0d1232f4fd3df7adac51486fb
GhostSI <ghostsi@reaperforge.com>
2022-05-21 16:24:33 +0200

    updated build script for tools

--- tools/build.bat
+++ tools/build.bat
@@ -1,5 +1,8 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
+cl.exe -std:c++latest gear.cpp
+del gear.obj
+
 cl.exe -std:c++latest obj.cpp
 del obj.obj
 
--- tools/build.sh
+++ tools/build.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
+g++ gear.cpp -o gear.out
 g++ obj.cpp -o obj.out
 g++ png.cpp -o png.out
